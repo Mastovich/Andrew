92ddcda291ba21b8c7fa76daa581bad2cf439dc1
Mastovich <113970230+Mastovich@users.noreply.github.com>
2022-09-22 17:44:51 +0200

    Add files via upload

---
+++ src/by/epam/tr/main/Fun6.java
@@ -0,0 +1,23 @@
+package by.epam.tr.main;
+
+public class Fun6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a;
+		double b;
+		double c;
+		
+		double z;
+		
+		a = 6;
+		b = 5;
+		c = 3;
+		
+		z = ((a -3) * b / 2) + c;
+		
+		System.out.println("z =" + z);
+
+	}
+
+}
---
+++ src/by/epam/tr/main/Name1.java
@@ -0,0 +1,18 @@
+package by.epam.tr.main;
+
+public class Name1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("M");
+		System.out.println("	A");
+		System.out.println("S");
+		System.out.println("	T");
+		System.out.println("O");
+		System.out.println("	V");
+		System.out.println("I");
+		System.out.println("	C");
+		System.out.println("H");
+	}
+
+}
